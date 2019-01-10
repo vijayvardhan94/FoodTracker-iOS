bb72049877d575aa8f89d4c0955cb97d2ec86578
Vijay Tadimeti <tadimetv@oregonstate.edu>
2019-01-10 14:49:01 -0800

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by vijay_tadimeti on 1/10/19.
+//  Copyright © 2019 vijay_tadimeti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
