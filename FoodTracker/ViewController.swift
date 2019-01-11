d8bcca2e09f7bd55591699310e3665622d9cf52f
Vijay Tadimeti <tadimetv@oregonstate.edu>
2019-01-10 16:16:25 -0800

    added custom field

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -26,6 +26,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
         textField.resignFirstResponder()
         return true
     }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
 //MARK: Actions
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         mealNameLabel.text = "Default Text"
